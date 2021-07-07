22597f38bcf96df4c6ad288b8ef8e39ed9e6a15d
DEEPANSHU GUPTA <deepanshu.gupta_cs18@gla.ac.in>
2021-07-07 22:06:18 +0530

    Add files via upload

---
+++ HelloWorld_1.js.js
@@ -0,0 +1,15 @@
+//To print 5 times * in a single line
+
+for (i = 0; i <= 0; i++){
+    var x = "";
+
+    for (j = 0; j <= 4; j++){
+        x=x+" *";
+    }
+
+console.log(x);
+}
+
+
+//OUTPUT
+//  * * * * *
